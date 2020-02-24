b3af491f0422f567468e2f84ed98dca67b49ead2
Mario Wagner <ma_wa@gmx.at>
2020-02-24 15:03:29 +0100

    Created first logic

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -1,9 +1,26 @@
 package com.codersbay;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println("Hello Fibonacci");
+
+        Scanner scan = new Scanner(System.in);
+        Integer n = scan.nextInt();
+        System.out.println(getNthFibonacci(n));
+
+    }
+
+    private static Integer getNthFibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return getNthFibonacci(n - 1) + getNthFibonacci(n - 2);
+
+
     }
 
 }
